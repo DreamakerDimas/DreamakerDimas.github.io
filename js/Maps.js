5e9b314002165b62508b711720f99776cf6631af
DreamakerDimas <dart.dimas@gmail.com>
2020-04-12 16:53:03 +0300

    add map interface v0.1

--- js/Maps.js
+++ js/Maps.js
@@ -1,4 +1,6 @@
 jQuery('document').ready(function() {
+let mapMultCoef = 1.0;
+let mapSizeCounter = 0;
 
 $(".NavButton")
 	.each(function(i) {
@@ -28,12 +30,43 @@ $('.Country').hover(
 		}
 	)
 ;
-
-	function mapSize(){
-	$("#svgMap").outerWidth($("#mapImage").width() * 1.0315);
-	$("#svgMap").outerHeight($("#mapImage").height() * 1.0315);
-	}
-	$(window).on('resize',mapSize);
 	mapSize();
+	function mapSizeChange (isZoomIn){
+    	if(isZoomIn === true && mapSizeCounter < 15){
+    		mapMultCoef += 0.2;
+    		mapSizeCounter++;
+		}
+    	else if(isZoomIn === false && mapSizeCounter > -1){
+    		mapMultCoef -= 0.2;
+			mapSizeCounter--;
+    	}
+    	else{
+    		return 0;
+		}
+        console.log('Coef' + mapMultCoef);
+    	mapSize();
+    	mapMultCoef = 1.0;
+    }
+
+    
+	function mapSize(){
+
+		$("#svgMap").outerWidth($("#mapImage").width() * 1.0315 * mapMultCoef);
+		$("#svgMap").outerHeight($("#mapImage").height() * 1.0315 * mapMultCoef);
+		$('#mapImage').outerWidth($('#mapImage').width()*mapMultCoef);
+		$('#mapImage').outerHeight($('#mapImage').height()*mapMultCoef);
+		$('#maps1').outerHeight($('#maps1').width());
+		console.log('Multy=' + mapMultCoef);
+
+		return 0;
+	}
+
+	$(window).on('resize',mapSize);
+
+
+	document.getElementById("mapSizePlus").addEventListener('click', function(){mapSizeChange(true);});
+	document.getElementById("mapSizeMinus").addEventListener('click', function(){mapSizeChange(false);});
+
 });
-	
+
+
